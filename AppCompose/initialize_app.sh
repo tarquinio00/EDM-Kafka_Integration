11b2de942fa3c9d97aa7cf3e43eb355a21bf044d
tarquinio00 <tarquinio.denicola.00@gmail.com>
2024-06-22 18:39:44 +0200

    Update initialization file

--- AppCompose/initialize_app.sh
+++ AppCompose/initialize_app.sh
@@ -1,5 +1,21 @@
 #!/bin/bash
 
+
+export $(grep -v '^#' .env | xargs)
+
+#create the TRACEABILITY external network if it doesn't exists
+if [ ! "$(docker network ls | grep ${TRACEABILITY_NETWORK})" ]; then
+  echo "Creating ${TRACEABILITY_NETWORK} network ..."
+  docker network create --driver bridge ${TRACEABILITY_NETWORK}
+fi
+
+#create the PROXY external network if it doesn't exists
+if [ ! "$(docker network ls | grep ${PROXY_NETWORK})" ]; then
+  echo "Creating ${PROXY_NETWORK} network ..."
+  docker network create --driver bridge ${PROXY_NETWORK}
+fi
+
+
 echo "Uploading Keycloak and Counter App images..."
 
 # Check if Keycloak image exists
@@ -17,3 +33,6 @@ if [[ "$(docker images -q counters-app:vf1.0.1 2> /dev/null)" == "" ]]; then
 else
   echo "Counter App image already exists, skipping load."
 fi
+
+
+unset $(grep -v '^#' .env | sed -E 's/(.*)=.*/\1/' | xargs)
